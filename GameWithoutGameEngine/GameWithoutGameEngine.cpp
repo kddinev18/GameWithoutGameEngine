56f929c646ac52bf7eaaec3c84610efdd65270e3
kddinev18 <kddinev18@codingburgas.bg>
2022-08-19 13:55:41 +0300

    Initialsie the vertex buffer objects

--- GameWithoutGameEngine/GameWithoutGameEngine.cpp
+++ GameWithoutGameEngine/GameWithoutGameEngine.cpp
@@ -13,7 +13,10 @@ int main()
      0.5f, -0.5f, 0.0f,
      0.0f,  0.5f, 0.0f
     };
-    
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
 
     int width = 800;
     int height = 800;
