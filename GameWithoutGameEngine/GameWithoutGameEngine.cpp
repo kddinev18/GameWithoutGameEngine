ac17872a673166a0a35987b77e997d01bef5ae24
kddinev18 <kddinev18@codingburgas.bg>
2022-08-21 14:13:57 +0300

    Change fragment shader

--- GameWithoutGameEngine/GameWithoutGameEngine.cpp
+++ GameWithoutGameEngine/GameWithoutGameEngine.cpp
@@ -8,20 +8,24 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char* vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
+    "   gl_Position = vec4(aPos, 1);\n"
+    "   ourColor = aColor;\n"
+    "}\n\0";
 const char* fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
+    "in vec3 ourColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vec4(ourColor, 1.0);\n"
     "}\n\0";
 
-unsigned int createVertexShader();
-unsigned int createFragmentShader();
-unsigned int createShaderProgram(unsigned int vertexShader, unsigned int fragmentShader);
+unsigned int createShaderProgram(const char* vertexShaderSource, const char* fragmentShaderSource);
+void processInput(GLFWwindow* window, float* vertices);
+void moveUp(float* vertices);
 
 int main()
 {
@@ -47,31 +51,42 @@ int main()
         return -1;
     }
 
-    unsigned int vertexShader = createVertexShader();
-    unsigned int fragmentShader = createFragmentShader();
-    unsigned int shaderProgram = createShaderProgram(vertexShader, fragmentShader);
+    unsigned int shaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
 
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, 
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+        // positions         // colors
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
     };
 
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
+    unsigned int VBO;
     glGenBuffers(1, &VBO);
-    glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
     while (!glfwWindowShouldClose(window))
     {
+        processInput(window, vertices);
+
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+
+        /*float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);*/
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
@@ -87,59 +102,71 @@ int main()
     return 0;
 }
 
-unsigned int createVertexShader()
+unsigned int createShaderProgram(const char* vertexShaderSource, const char* fragmentShaderSource)
 {
+    int success;
+
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
-
-    int success;
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (success)
-    {
-        return vertexShader;
-    }
-    else
-    {
+    if (!success)
         throw "ERROR::SHADER::VERTEX::COMPILATION_FAILED";
-    }
-}
 
-unsigned int createFragmentShader()
-{
+
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
-
-    int success;
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (success)
-    {
-        return fragmentShader;
-    }
-    else
-    {
+    if (!success)
         throw "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED";
-    }
-}
 
-unsigned int createShaderProgram(unsigned int vertexShader, unsigned int fragmentShader)
-{
+
     unsigned int shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
-
-    int success;
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (success)
-    {
-        return shaderProgram;
-    }
-    else
-    {
+    if (!success)
         throw "ERROR::SHADER::PROGRAM::LINKING_FAILED";
+    else
+        return shaderProgram;
+}
+
+void processInput(GLFWwindow* window, float* vertices)
+{
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        moveUp(vertices);
+}
+
+void moveUp(float* vertices)
+{
+    for (size_t i = 0; i < 9; i = i + 3)
+    {
+        if (vertices[i + 1] != 0)
+        {
+            vertices[i + 1] += 0.1f;
+            if (vertices[i + 1] > 1)
+            {
+                vertices[i + 1] = 1;
+            }
+        }
     }
+}
+
+void moveDown(float* vertices)
+{
+
+}
+
+void moveRight(float* vertices)
+{
+
+}
+
+void moveLeft(float* vertices)
+{
+
 }
