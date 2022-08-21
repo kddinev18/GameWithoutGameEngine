c8658e21267580d3f59a9ed353e4226a1edd2b24
kddinev18 <kddinev18@codingburgas.bg>
2022-08-21 14:33:23 +0300

    Implement Shader class

--- GameWithoutGameEngine/GameWithoutGameEngine.cpp
+++ GameWithoutGameEngine/GameWithoutGameEngine.cpp
@@ -1,29 +1,16 @@
 #include <GL\glew.h>
 #include <glfw3.h>
 #include <iostream>
+#include <string>
+#include "Shader.h"
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char* vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;\n"
-    "out vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos, 1);\n"
-    "   ourColor = aColor;\n"
-    "}\n\0";
-const char* fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "in vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(ourColor, 1.0);\n"
-    "}\n\0";
+std::string vertexShaderSourcePath = "Shaders\\VertextShader.txt";
+std::string fragmentShaderSourcePath = "Shaders\\FragmentShader.txt";
 
-unsigned int createShaderProgram(const char* vertexShaderSource, const char* fragmentShaderSource);
 void processInput(GLFWwindow* window, float* vertices);
 void moveUp(float* vertices);
 
@@ -51,7 +38,7 @@ int main()
         return -1;
     }
 
-    unsigned int shaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
+    Shader* shader = new Shader(vertexShaderSourcePath, fragmentShaderSourcePath);
 
 
     float vertices[] = {
@@ -82,13 +69,7 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-
-        /*float timeValue = glfwGetTime();
-        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);*/
-
-        glUseProgram(shaderProgram);
+        shader->use();
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
@@ -97,42 +78,9 @@ int main()
     }
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
     glfwTerminate();
+    delete shader;
     return 0;
-}
-
-unsigned int createShaderProgram(const char* vertexShaderSource, const char* fragmentShaderSource)
-{
-    int success;
-
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-        throw "ERROR::SHADER::VERTEX::COMPILATION_FAILED";
-
-
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-        throw "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED";
-
-
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success)
-        throw "ERROR::SHADER::PROGRAM::LINKING_FAILED";
-    else
-        return shaderProgram;
 }
 
 void processInput(GLFWwindow* window, float* vertices)
